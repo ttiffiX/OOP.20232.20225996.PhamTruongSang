81f2563c1a0a5dd3ab6f3bb3973dd05fb1b87a66
ttiffiX <sangpham1224@gmail.com>
2024-03-12 11:20:45 +0700

    lab01

--- Lab01/HelloWorld.java
+++ Lab01/HelloWorld.java
@@ -3,5 +3,6 @@ package Lab01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        System.out.println("Xin chao thay \n va cac ban");
     }
 }
