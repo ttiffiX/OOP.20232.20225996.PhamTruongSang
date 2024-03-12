47ee29af2b32877dc68b6daf1756eb8a8c5fe6ca
ttiffiX <sangpham1224@gmail.com>
2024-03-12 11:31:59 +0700

    lab01

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,17 @@
+package Lab01;
+
+import javax.swing.*;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Hello my friend, u doing good?");
+        String result;
+        result = JOptionPane.showInputDialog("Please enter your name:");
+        JOptionPane.showMessageDialog(null, "Hi " + result + "!");
+        System.exit(0);
+
+    }
+}
+
+
