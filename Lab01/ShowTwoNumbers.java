254956ff0d57f619505fd1e216e768168b72c85a
ttiffiX <sangpham1224@gmail.com>
2024-03-12 11:40:49 +0700

    lab01

---
+++ Lab01/ShowTwoNumbers.java
@@ -0,0 +1,16 @@
+package Lab01;
+
+import javax.swing.*;
+
+public class ShowTwoNumbers {
+    public static void main(String[] args) {
+        String strNum1, strNum2;
+        String strNotification = "Bạn vừa nhập: ";
+        strNum1 = JOptionPane.showInputDialog(null, "Vui lòng nhập số đầu tiên: ", "Nhập số đầu tiên", JOptionPane.INFORMATION_MESSAGE);
+        strNotification += strNum1 + " và ";
+        strNum2 = JOptionPane.showInputDialog(null, "Vui lòng nhập số thứ hai: ", "Nhập số thứ hai", JOptionPane.INFORMATION_MESSAGE);
+        strNotification += strNum2;
+        JOptionPane.showMessageDialog(null, strNotification, "Hiển thị hai số", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
