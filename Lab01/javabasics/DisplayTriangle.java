66d6d7b9ef7d188319ac761a99f8c9494336d233
ttiffiX <sangpham1224@gmail.com>
2024-03-13 21:26:02 +0700

    Lab01

---
+++ Lab01/javabasics/DisplayTriangle.java
@@ -0,0 +1,28 @@
+package Lab01.javabasics;
+
+import java.util.Scanner;
+
+public class DisplayTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.print("Enter n = ");
+        n = input.nextInt();
+
+        for (int i = 1; i <= 2 * n; i += 2) {
+            int j = (2 * n - i) / 2, k = i;
+            while (j > 0) {
+                System.out.print(" ");
+                j--;
+            }
+            while (k > 0) {
+                System.out.print("*");
+                k--;
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
