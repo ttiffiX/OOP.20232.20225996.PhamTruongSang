571b2757ac3ceb7038938f1322a5217056862712
ttiffiX <sangpham1224@gmail.com>
2024-03-28 23:51:44 +0700

    Summit Exercise 15

---
+++ Lab02/AimsProject/src/TestPassingParameter.java
@@ -0,0 +1,29 @@
+package Lab02.AimsProject.src;
+
+public class TestPassingParameter {
+    public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+        DVD jungleDVD = new DVD("Jungle");
+        DVD cinderellaDVD = new DVD("Cinderella");
+
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
+
+    public static void changeTitle(DVD dvd, String title) {
+//        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+//        dvd = new DVD(oldTitle);
+    }
+}
