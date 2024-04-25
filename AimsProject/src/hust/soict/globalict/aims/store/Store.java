ba43ed0066e8ff11d38bd844ba28c8c3c5fd0e47
ttiffiX <sangpham1224@gmail.com>
2024-04-25 23:37:30 +0700

    update store to work with media

--- AimsProject/src/hust/soict/globalict/aims/store/Store.java
+++ AimsProject/src/hust/soict/globalict/aims/store/Store.java
@@ -1,42 +1,33 @@
 package AimsProject.src.hust.soict.globalict.aims.store;
 
 import AimsProject.src.hust.soict.globalict.aims.media.DVD;
+import AimsProject.src.hust.soict.globalict.aims.media.Media;
+
+import java.util.ArrayList;
 
 public class Store{
     public static final int MAX_NUMBERS_DVD = 100;
-    DVD itemInStore[] = new DVD[MAX_NUMBERS_DVD];
+    private ArrayList<Media> itemInStore = new ArrayList<Media>();
     private int qtyItem = 0;
 
-    public void addDVDts(DVD disc) {
-        if (qtyItem > MAX_NUMBERS_DVD) {
-            System.out.println("The store is almost full.");
-        } else {
-            for (int i = 0; i < qtyItem; i++) {
-                if (itemInStore[i].isMatch(disc)) {
-                    System.out.println("The DVD already exist.");
-                    return;
-                }
-            }
-            itemInStore[qtyItem++] = disc;
-            System.out.println("The DVD: '" + disc.getTitle() + "' has been added.");
-        }
+    public void addMedia(Media media) {
+        if (!itemInStore.contains(media)) {
+            itemInStore.add(media);
+            System.out.println("The media has been added.");
+        }else System.out.println("The media is already exist.");
     }
 
-    public void removeDVDfs(DVD disc) {
-        if (qtyItem == 0) System.out.println("The store is empty.");
+    public void removeMedia(Media media) {
+        if (itemInStore.isEmpty()) System.out.println("The Cart is empty.");
         else {
-            for (int i = 0; i < qtyItem - 1; i++) {
-                if (itemInStore[i].equals(disc)) {
-                    while (i < qtyItem - 1) {
-                        itemInStore[i] = itemInStore[i + 1];
-                        i++;
-                    }
-                    qtyItem--;
-                    System.out.println("The disc " + disc.getTitle() + " has been removed.");
+            for (Media i : itemInStore) {
+                if (i.equals(media)) {
+                    itemInStore.remove(media);
+                    System.out.println("The media has been removed.");
                     return;
                 }
             }
-            System.out.println("Nothing to remove.");
+            System.out.println("The media does not exist.");
         }
     }
 }
