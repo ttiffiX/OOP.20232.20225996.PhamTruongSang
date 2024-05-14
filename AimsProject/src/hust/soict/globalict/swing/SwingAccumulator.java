04403b1b2d53d94d83d133edaffcbf1b8914aca6
ttiffiX <sangpham1224@gmail.com>
2024-05-14 11:05:52 +0700

    Learn about AWT and Swing

---
+++ AimsProject/src/hust/soict/globalict/swing/SwingAccumulator.java
@@ -0,0 +1,48 @@
+package AimsProject.src.hust.soict.globalict.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingAccumulator extends JFrame {
+    private JTextField tfInput;
+    private JTextField tfOutput;
+    private int sum = 0;
+
+    public SwingAccumulator() {
+        Container cp = getContentPane();
+        cp.setLayout(new GridLayout(2, 2));
+
+        cp.add(new JLabel("Enter an Tnteger: "));
+
+        tfInput = new JTextField(10);
+        cp.add(tfInput);
+        tfInput.addActionListener(new TFInputListener());
+
+        cp.add(new JLabel("The Accumutated Sum is: "));
+
+        tfOutput = new JTextField(10);
+        tfOutput.setEditable(false);
+        cp.add(tfOutput);
+
+        setTitle("Swing AccumuLator");
+        setSize(500, 200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new SwingAccumulator();
+    }
+
+    private class TFInputListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+            int numberIn = Integer.parseInt(tfInput.getText());
+            sum += numberIn;
+            tfInput.setText("");
+            tfOutput.setText(sum + "");
+        }
+    }
+}
